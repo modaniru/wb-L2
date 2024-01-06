65116f2e950cb8f98efdb4ae7ff6e7408f121308
aleksejmoksancev <mokshantsev.aleksei>
2024-01-07 03:03:15 +0500

    dev06-07

--- develop/dev07/task.go
+++ develop/dev07/task.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
 /*
 === Or channel ===
 
@@ -33,6 +39,50 @@ start := time.Now()
 fmt.Printf(“fone after %v”, time.Since(start))
 */
 
-func main() {
+func getChan(name string) <- chan string{
+	ch := make(chan string, 10)
+	go func(){
+		for i := 0; i < 5; i++{
+			time.Sleep(time.Second * time.Duration((rand.Intn(10) + 1)))
+			ch <- name
+		}
+		close(ch)
+	}()
+	return ch
+}
 
+func or(channels ... <- chan string) chan string{
+	ch := make(chan string)
+	closeChan := make(chan struct{})
+	for _, c := range channels{
+		go func(c <-chan string){
+			for v := range c{
+				ch <- v
+			}
+			closeChan <- struct{}{}
+		}(c)
+	}
+	go func(){
+		count := 0
+		for range closeChan {			
+			count++
+			if count == len(channels){
+				close(ch)
+				return
+			}
+		}
+	}()
+	return ch
+}
+
+func main() {
+	ch1 := getChan("n1")
+	ch2 := getChan("n2")
+	ch3 := getChan("n3")
+
+	m := or(ch1, ch2, ch3)
+
+	for v := range m{
+		fmt.Println(v)
+	}
 }
