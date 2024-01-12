e68a1ac84622a4a159f7f9721c02c9a1c113c0cc
modaniru <xmoon4ek@gmail.com>
2024-01-13 01:51:26 +0500

    dev09

--- develop/dev09/task.go
+++ develop/dev09/task.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"errors"
+	"flag"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
 /*
 === Утилита wget ===
 
@@ -9,5 +19,43 @@ package main
 */
 
 func main() {
+	flag.Parse()
+	link := flag.Arg(0)
+	
+	resp, err := http.Get(link)
+	if err != nil{
+		log.Fatal(err.Error())
+	}
 
+	filename, err := getFileName(link)
+	if err != nil{
+		log.Fatal(err.Error())
+	}
+
+	err = os.Mkdir(filename, os.ModePerm)
+	if err != nil{
+		log.Fatal(err.Error())
+	}
+	// strings.Split(args[1], "/")[0]
+	bytes, err := io.ReadAll(resp.Body)
+	if err != nil{
+		log.Fatal(err.Error())
+	}
+
+	f, err := os.Create(filename + "/index.html")
+	if err != nil{
+		log.Fatal(err.Error())
+	}
+	defer f.Close()
+
+	f.WriteString(string(bytes))
+}
+
+func getFileName(link string) (string, error){
+	args := strings.Split(link, "//")
+	if len(args) < 2{
+		return "", errors.New("not enough arguments")
+	}
+
+	return strings.Split(args[1], "/")[0], nil
 }
