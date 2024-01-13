549cce4073edabe97b81e1ee971e7d0cc7fe0146
aleksejmoksancev <mokshantsev.aleksei>
2024-01-14 01:08:52 +0500

    dev08

--- develop/dev08/task.go
+++ develop/dev08/task.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
 /*
 === Взаимодействие с ОС ===
 
@@ -15,5 +24,50 @@ package main
 */
 
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("> ")
+		
+		if !scanner.Scan(){
+			break
+		}
 
+		input := scanner.Text()
+		commands := strings.Split(input, " | ")
+		for _, command := range commands{
+			execCommand(command)
+		}
+	}
+}
+
+func execCommand(command string){
+	args := strings.Split(command, " ")
+	switch args[0]{
+	case "cs":
+	case "ls":
+	case "pwd":
+	case "echo":
+	case "kill":
+	case "ps":
+	case "quit":
+		fmt.Println("Bye Bye!")
+		os.Exit(0)
+	default:
+		fmt.Println("unexcepted command!")
+		return
+	}
+
+	cmd := exec.Command(args[0], args[1:]...)
+	err := cmd.Err
+	if err != nil{
+		log.Fatal(err.Error())
+	}
+
+	bytes, err := cmd.Output()
+	if err != nil{
+		log.Fatal(err.Error())
+	}
+
+	str := string(bytes)
+	fmt.Println(str[:len(str) - 1])
 }
