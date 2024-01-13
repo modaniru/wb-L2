8c6b689e5ae426884a15904a1d0275e117a538ae
aleksejmoksancev <mokshantsev.aleksei>
2024-01-13 23:32:12 +0500

    telnet

--- develop/dev10/task.go
+++ develop/dev10/task.go
@@ -1,20 +1,61 @@
 package main
 
-/*
-=== Утилита telnet ===
-
-Реализовать примитивный telnet клиент:
-Примеры вызовов:
-go-telnet --timeout=10s host port go-telnet mysite.ru 8080 go-telnet --timeout=3s 1.1.1.1 123
-
-Программа должна подключаться к указанному хосту (ip или доменное имя) и порту по протоколу TCP.
-После подключения STDIN программы должен записываться в сокет, а данные полученные и сокета должны выводиться в STDOUT
-Опционально в программу можно передать таймаут на подключение к серверу (через аргумент --timeout, по умолчанию 10s).
-
-При нажатии Ctrl+D программа должна закрывать сокет и завершаться. Если сокет закрывается со стороны сервера, программа должна также завершаться.
-При подключении к несуществующему сервер, программа должна завершаться через timeout.
-*/
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
 func main() {
+	// Парсим аргументы командной строки
+	host := flag.String("host", "", "Target host (IP or domain name)")
+	port := flag.Int("port", 0, "Target port")
+	timeout := flag.Duration("timeout", 10*time.Second, "Connection timeout")
+	flag.Parse()
 
+	// Проверяем наличие обязательных аргументов
+	if *host == "" || *port == 0 {
+		fmt.Println("Usage: go-telnet --host <host> --port <port> [--timeout <timeout>]")
+		os.Exit(1)
+	}
+
+	// Формируем адрес сервера
+	serverAddr := fmt.Sprintf("%s:%d", *host, *port)
+
+	// Устанавливаем таймаут на подключение
+	conn, err := net.DialTimeout("tcp", serverAddr, *timeout)
+	if err != nil {
+		fmt.Printf("Error connecting to %s: %v\n", serverAddr, err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	// Канал для отслеживания завершения программы
+	done := make(chan struct{})
+
+	// Запускаем горутину для обработки ввода из STDIN
+	go func() {
+		io.Copy(conn, os.Stdin)
+		done <- struct{}{}
+	}()
+
+	// Запускаем горутину для обработки вывода в STDOUT
+	go func() {
+		io.Copy(os.Stdout, conn)
+		done <- struct{}{}
+	}()
+
+	// Ожидаем завершения программы по сигналам или закрытию сокета
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+
+	select {
+	case <-done:
+	case <-signals:
+	}
 }
